328ecb001f7bbec7690be1f873b0fbae6832e5f5
Vipul Jha <vipuljha08@gmail.com>
2019-05-16 22:17:56 +0530

    MainActivity: Add main functionality to app It will randomly generate a number and will show image accordingly.
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,37 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+     ImageView answerImage;
+     Button askButton;
+     final int[] imageArray = {
+             R.drawable.ball1,
+             R.drawable.ball2,
+             R.drawable.ball3,
+             R.drawable.ball4,
+             R.drawable.ball5
+     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerImage = findViewById(R.id.answer);
+        askButton = findViewById(R.id.ask_button);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random genRandom = new Random();
+                int number = genRandom.nextInt(5);
+                answerImage.setImageResource(imageArray[number]);
+            }
+        });
     }
 }
